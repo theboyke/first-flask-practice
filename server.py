155bad824a9c1d379ad0a0049af42b44baaf8afe
Boyke Wieberdink <Boyke@MacBook-Pro-van-MacBook.local>
2019-11-06 14:53:24 +0800

    intial commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return '<h1>Why so easy</h1>'
+
+
+@app.route("/another")
+def show():
+    return '<h1>Yo</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
