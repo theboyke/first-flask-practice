26f262ad8b9ed9500d2daf23304c013b775075e0
Boyke Wieberdink <Boyke@MacBook-Pro-van-MacBook.local>
2019-11-06 16:12:29 +0800

    added dynamic routing

--- server.py
+++ server.py
@@ -7,10 +7,15 @@ def index():
     return '<h1>Why so easy</h1>'
 
 
-@app.route("/another")
-def show():
-    return '<h1>Yo</h1>'
+# @app.route("/another")
+# def show():
+#     return '<h1>Yo</h1>'
+
+
+@app.route('/user/<username>')
+def show(username):
+    return f"Hi {username[0:5]}"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
