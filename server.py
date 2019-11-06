e9c0b49bfe0a9b042d0a5c2b951b21f5aa514814
Boyke Wieberdink <Boyke@MacBook-Pro-van-MacBook.local>
2019-11-06 19:28:37 +0800

    all day 4 exercises

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/<name>")
+def index(name):
+    name = name.upper()
     # by default looks for index.html inside a templates folder in the same directory as this script.
-    return render_template('index.html')
+    signed_in = True
+    return render_template('index.html', name=name, signed_in=signed_in)
 
 
 # @app.route("/another")
@@ -15,7 +17,12 @@ def index():
 
 @app.route('/user/<username>')
 def show(username):
-    return f"Hi {username[5]}"
+    return f"Hi {username[2]}"
+
+
+@app.route('/')
+def home():
+    return 'This is the homescreen!'
 
 
 if __name__ == '__main__':
